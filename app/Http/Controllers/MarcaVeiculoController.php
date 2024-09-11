5a19de559c748bc3cf1987c229d8a6a19d213413
azevedo2elve <azevedogabriel00@gmail.com>
2024-09-10 23:44:37 -0300

    Mostrando as marca/modelo e colocando ação de deletar

--- app/Http/Controllers/MarcaVeiculoController.php
+++ app/Http/Controllers/MarcaVeiculoController.php
@@ -11,20 +11,32 @@ class MarcaVeiculoController extends Controller
          // Verifica se há filtros passados na requisição
          $query = MarcaVeiculo::query();
 
+        //  dd($request->has('marca'));
          // Aplicar filtro de marca, se houver
          if ($request->has('marca')) {
-             $query->where('marca', 'like', '%' . $request->input('marca') . '%');
+            $marca = $request->input('marca');
+            // Usar upper() e comparar case-insensitive
+            $query->whereRaw('upper(descricao) like upper(?)', ['%' . $marca . '%']);
          }
 
-         // Aplicar filtro de modelo, se houver
-         if ($request->has('modelo')) {
-             $query->where('modelo', 'like', '%' . $request->input('modelo') . '%');
-         }
+        // Aplicar filtro para garantir que data_exclusao seja null
+        $query->whereNull('data_exclusao');
 
          // Obter resultados paginados ou todos, conforme necessidade
-         $resultados = $query->paginate(10); // ou use ->get() para obter todos os resultados
+         $resultados = $query->paginate(15); // ou use ->get() para obter todos os resultados
 
+        //  dd($resultados);
          // Passar os resultados para a view
          return view('marcaveiculo', compact('resultados'));
+    }
+
+    public function destroy($id)
+    {
+        // Encontra o veículo pelo ID e o deleta
+        $veiculo = MarcaVeiculo::findOrFail($id);
+        $veiculo->delete();
+
+        // Redireciona com uma mensagem de sucesso
+        return redirect()->route('marcaveiculo.index')->with('success', 'Veículo deletado com sucesso!');
     }
 }
